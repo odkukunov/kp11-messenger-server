d71511b2b10910276171b248a41bc4cf874cb503
kukun <kukun@DESKTOP-KL1EPST>
2022-04-20 18:27:42 +0300

    Added Dist branch

--- src/chats/ChatDTO.ts
+++
@@ -1,9 +0,0 @@
-export type ChatDTO = {
-  name: string;
-  users?: Array<string>;
-};
-
-export type ChatUpdateDTO = {
-  name: string;
-  avatar: string;
-};
--- src/chats/chats-messages/MessageDTO.ts
+++
@@ -1,3 +0,0 @@
-export type CreateMessageDTO = {
-  content: string;
-};
